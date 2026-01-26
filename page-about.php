17e33f35b82dd890fbf822298ba2e9f0e265faa5
Orik Galica <galica.orik@gmail.com>
2026-01-26 20:25:44 +0100

    1

---
+++ page-about.php
@@ -0,0 +1,62 @@
+<?php
+
+
+/*
+
+Template Name: About Page
+
+*/
+
+
+?>
+
+<?php get_header() ?>
+
+<div class="site-content" id="content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="contaniner">
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Tempore explicabo quasi fugiat nisi voluptatibus, </p>
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who we are</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, ea!</p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="contanier">
+                <h2>Our Mission</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Suscipit magni rem fugit nam veritatis dicta cumque blanditiis ullam! Tempore, aut?</p>
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What we cover</h2>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                <h2>Want to Connect</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
+            </div>
+        </section>
+
+    </main>
+</div>
+
+<?php get_footer(); ?>
